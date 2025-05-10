2037e5201892ed71b9af8fec7cc067119d85ee31
swqolx <153764765+swqolx@users.noreply.github.com>
2025-05-10 08:52:01 +0200

    1

---
+++ hm7.js
@@ -0,0 +1,57 @@
+//ex 1
+// const numbers = [1, 8, 21];
+// numbers[1] = 10;
+// console.log(numbers);
+
+
+//ex2
+// const text = ['lala', 'lolo', 'lele','lulu']
+// text[4] = 'lili'
+// console.log(text);
+
+
+//ex3
+// const numbers = [11, 43, 2];
+// let sum = 0;
+// for(const num of numbers) {
+//    sum += num
+// }
+// console.log(sum);
+
+
+//ex4
+// const numbers = [2, 76, 82, 3, 21] 
+// for (let i = 0; i < numbers.length; i += 1) {
+//     console.log(numbers[i]);
+// }
+
+
+//ex5
+// const italianCities = [Varenna, Rome, Venice, Milano, Monzo];
+// for (let i = 0; i < italianCities.length; i += 1) {
+//     if (italianCities[i].length > 5) {
+//         console.log(italianCities[i])
+//     }
+// }
+
+
+//ex6
+// const numbers = [1, 45, 26, 88, 90, 300, 15, 3, 53, 700];
+// let maxNumber = numbers[0];
+// for (let i = 1; i < numbers.length; i += 2) {
+//     if (numbers[i] > maxNumber) {
+//         maxNumber = numbers[i];
+//     }
+// }
+// console.log(`найбільше число - ${maxNumber}`);
+
+
+//ex7
+// const numbers = [3, 77,56,22,89,4,7,33,67,100,432,673,911,231,211];
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 === 0) {
+//         console.log(numbers[i]);
+//     } else {
+//         continue;
+//     }
+// }
